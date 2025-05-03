8455de64d2e1c752f1c4273a9307bee7081316b3
Кирилл Терских <kirillterskih@MacBook-Air-Kirill.local>
2025-05-04 00:53:12 +0300

    Added paje object test

---
+++ pages/main_page.py
@@ -0,0 +1,9 @@
+from .base_page import BasePage
+
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element("css selector", "#login_link")
+        login_link.click()
+
+    def should_be_login_link(self):
+        self.browser.find_element("css selector", "#login_link_invalid")
