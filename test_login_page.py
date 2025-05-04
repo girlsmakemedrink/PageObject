dd1f43dba7b5234e5470868c84c9a8a9252696f3
Кирилл Терских <kirillterskih@MacBook-Air-Kirill.local>
2025-05-04 17:46:40 +0300

    minor link's name cnanging

--- test_login_page.py
+++ test_login_page.py
@@ -1,12 +1,15 @@
+from PageObject.pages.main_page import MainPage
 from pages.login_page import LoginPage
 
-LOGIN_LINK = "http://selenium1py.pythonanywhere.com"
+MAIN_PAGE = "http://selenium1py.pythonanywhere.com"
 LOGIN_PAGE = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
 
 def test_guest_can_go_to_login_page(browser):
-    page = LoginPage(browser, LOGIN_LINK)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page = MainPage(browser, MAIN_PAGE)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()       # выполняем метод страницы — проверяем урл страницы
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 def test_guest_should_see_login_form(browser):
     page = LoginPage(browser, LOGIN_PAGE)
